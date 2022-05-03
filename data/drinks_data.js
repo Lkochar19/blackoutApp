4cf1ffdd70ba46ea5f1db6eb513fa2ed34e194a7
Lucy Kocharian <Lk2583@nyu.edu>
2022-05-03 17:40:12 +0200

    drinks_data added

---
+++ data/drinks_data.js
@@ -0,0 +1,20 @@
+const drinks_data = [
+    {
+        id: 'vodka',
+        title: 'Vodka'
+    },
+    {
+        id: 'tequila',
+        title: 'Tequila'
+    },
+    {
+        id: 'gin',
+        title: 'Gin'
+    },    {
+        id: 'mix',
+        title: 'Mix It Up'
+    },    {
+        id: 'random',
+        title: 'Randomize'
+    },
+]
